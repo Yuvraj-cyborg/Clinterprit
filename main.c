154df92f1febd08eaf63fa0e21439539dcd03cc9
Yuvraj <yuvrajbiswalofficial@gmail.com>
2026-01-24 23:57:16 +0530

    feat: initiate

--- main.c
+++ main.c
@@ -1,13 +1,31 @@
 #include<stdio.h>
+#define INPUT_LEN 1024
 
-#define INPUT_LEN=1024;
+void calculate()
+
+void parse_expressions(char* input) {
+  int i=0;
+  while(input[i] != '\0') {
+    input
+  }
+}
+
+void interpret(char* input){
+  char c;
+  while((c = *input) != '\0') {
+    printf("%c\n",c);
+    input++;
+  }
+};
+
 
 int main() {
+  char buff[INPUT_LEN];
   printf("Clinterprit started...\n");
   while (1) {
-    char buff[INPUT_LEN];
     printf(">>>");
     fgets(buff,sizeof(buff),stdin);
+    interpret(buff);
   }
 
   return 0;
